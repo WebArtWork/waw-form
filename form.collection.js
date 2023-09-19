223544b8439682edbbe8f44d44f058d536eba42a
Nika <nikarogovska@gmail.com>
2023-09-19 14:07:25 +0300

    update code 2

--- form.collection.js
+++ form.collection.js
@@ -22,5 +22,5 @@ module.exports = function (waw) {
 		this.components = obj.components;
 	}
 
-	waw.Form = waw.mongoose.model('Form', Schema);
+	return waw.Form = waw.mongoose.model('Form', Schema);
 };
