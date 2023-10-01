6acced0ac4c6eb1ec1ce1cdc8bf7b1c0790ebb3c
Honchar Denys <crackeraki@gmail.com>
2023-10-01 16:54:01 +0300

    initialize core wjst

---
+++ temp/form.collection.js
@@ -0,0 +1,26 @@
+module.exports = function (waw) {
+	const Schema = waw.mongoose.Schema({
+		title: String,
+		formId: String,
+		active: Boolean,
+		components: [{
+			name: String,
+			key: String,
+			root: Boolean,
+			fields: [{
+				name: String,
+				value: String
+			}],
+			components: [{}]
+		}]
+	});
+
+	Schema.methods.create = function (obj, user, sd) {
+		this.title = obj.title;
+		this.active = !!obj.active;
+		this.formId = obj.formId;
+		this.components = obj.components;
+	}
+
+	return waw.Form = waw.mongoose.model('Form', Schema);
+};
